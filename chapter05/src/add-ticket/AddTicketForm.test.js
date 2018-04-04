a2201c4264f5395be3411866b018339c8a1e34b7
ralph14 <ralphr@packtpub.com>
2018-04-04 09:45:53 +0530

    Code files

--- chapter05/src/add-ticket/AddTicketForm.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddTicketForm from './AddTicketForm';
-
-it('Renders add ticket form component without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AddTicketForm />, div);
-});
--- chapter05/src/tickets-listing/ViewTickets.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ViewTicketTable from './ViewTicketTable.jsx';
-
-it('Renders view tickets component without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ViewTicketTable />, div);
-});
